0fc4f50f9bbc43d25ae96ab7956863db959b889c
ecasellas <enric.casellas@gmail.com>
2019-10-02 15:19:20 +0200

    Modifies distance to coast figure

--- envmodsoft/figures/fig_distance_coast.py
+++ envmodsoft/figures/fig_distance_coast.py
@@ -5,6 +5,7 @@ from osgeo import gdal
 import subprocess
 import os
 import geopandas as gp
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 
 def crop_to_cat(inRaster):
@@ -53,10 +54,41 @@ im1 = ax1.contourf(d_coast[0], d_coast[1], d_coast[2][:, :].T, levels=levels,
 ax1.set_aspect('equal')
 ax1.axis('off')
 
-cbar_ax = fig.add_axes([0.45, 0.20, 0.30, 0.015])
+cbar_ax = fig.add_axes([0.35, 0.16, 0.30, 0.015])
 cbar = plt.colorbar(im1, cax=cbar_ax, orientation='horizontal', ticks=levels)
 cbar.ax.tick_params(labelsize=3.5, width=0.3, size=2, pad=1)
 cbar.outline.set_linewidth(0.3)
 cbar.set_label('Distance to coast function', size=4)#(\u00b0C)', size=8)
 
-plt.savefig('/tmp/ems_distance_coast.pdf', bbox_inches='tight', pad=0)
+
+coast_line = gp.read_file('sample-data/explanatory/cat_coast_line.json')
+coast_line.plot(ax=ax1, color='red', linewidth=0.8, label='Coast line')
+ax1.legend(loc='lower right', fontsize=3.5, frameon=False)
+ax1.text(271030, 4742408, 'a)', size=4)
+
+mbaxes = inset_axes(ax1, width="100%", height="100%",
+                    bbox_to_anchor=[430000,4510040,120000,60000],
+                    bbox_transform=ax1.transData, loc='lower left')
+mbaxes.spines['bottom'].set_linewidth(0.1)
+mbaxes.spines['top'].set_linewidth(0.1)
+mbaxes.spines['right'].set_linewidth(0.1)
+mbaxes.spines['left'].set_linewidth(0.1)
+
+world = gp.read_file('envmodsoft/data/explanatory/world_boundaries.geojson')
+ax2 = world.plot(ax=mbaxes, facecolor='None', edgecolor='darkgrey', linewidth=0.3)
+ax2.set_xlim(-4, 15)
+ax2.set_ylim(36, 48)
+ax2.set_xticks([])
+ax2.set_yticks([])
+ax2.text(-5.7, 46.8, 'b)', size=4)
+
+catalonia = gp.read_file('envmodsoft/data/explanatory/cat_boundaries.geojson')
+catalonia.plot(ax=mbaxes, facecolor='darkgrey', edgecolor='None')
+
+
+
+
+ax1.set_xlim(260000, 530000)
+ax1.set_ylim(4488100, 4750000)
+
+plt.savefig('/tmp/ems_distance_coast.pdf', pad=0)
